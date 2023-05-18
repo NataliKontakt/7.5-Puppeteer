993417bf8a65f7d71ed892732578cf14952c1583
natali-kontakt <natalikontaktgood@gmail.com>
2023-05-18 20:58:18 +0500

    add task 2

--- features/step_definitions/search.steps.js
+++ features/step_definitions/search.steps.js
@@ -2,7 +2,9 @@ const puppeteer = require("puppeteer");
 const chai = require("chai");
 const expect = chai.expect;
 const { Given, When, Then, Before, After } = require("cucumber");
-const { putText, getText } = require("../../lib/commands.js");
+const {clickElement, getText } = require("../../lib/commands.js");
+
+
 
 Before(async function () {
   const browser = await puppeteer.launch({ headless: false, slowMo: 50 });
@@ -17,18 +19,67 @@ After(async function () {
   }
 });
 
-Given("user is on {string} page", async function (string) {
-  return await this.page.goto(`https://netology.ru${string}`, {
+Given("юзер находится на странице {string}", async function (string) {
+  return await this.page.goto(`http://qamid.tmweb.ru/client${string}`, {
     setTimeout: 20000,
   });
 });
 
-When("user search by {string}", async function (string) {
-  return await putText(this.page, "input", string);
+When("юзер возвращается на страницу {string}", async function (string) {
+  return await this.page.goto(`http://qamid.tmweb.ru/client${string}`, {
+    setTimeout: 20000,
+  });
 });
 
-Then("user sees the course suggested {string}", async function (string) {
-  const actual = await getText(this.page, "a[data-name]");
-  const expected = await string;
-  expect(actual).contains(expected);
+When("юзер выбирает {int}-й день", async function (int) {
+        return await clickElement(this.page, `body > nav > a:nth-child(${int}) > span.page-nav__day-week`);
+});
+When("юзер выбирает время", async function () {
+  return await clickElement(this.page, "[data-seance-id='156'");
+});
+
+When("юзер выбирает {int}-й ряд {int} место", async function (int1, int2) {
+  await clickElement(
+    this.page,
+    `div.buying-scheme__wrapper > div:nth-child(${int1}) > span:nth-child(${int2})`
+  );
+  await clickElement(this.page,".acceptin-button");
+ 
+});
+When("юзер выбирает {int}-й ряд {int},{int} место", async function (int1, int2, int3) {
+  await clickElement(
+    this.page,
+    `div.buying-scheme__wrapper > div:nth-child(${int1}) > span:nth-child(${int2})`
+  );
+  await clickElement(
+    this.page,
+    `div.buying-scheme__wrapper > div:nth-child(${int1}) > span:nth-child(${int3})`
+  );
+  await clickElement(this.page,".acceptin-button");
+ 
+});
+When("юзер повторно выбирает {int}-й ряд {int} место", async function (int1, int2) {
+  await clickElement(
+    this.page,
+    `div.buying-scheme__wrapper > div:nth-child(${int1}) > span:nth-child(${int2})`
+  );
+
+ 
+});
+When("юзер нажимает Получить код бронирования", async function () {
+  await clickElement(this.page,".acceptin-button");
+});
+
+Then("юзер видит сообщение Вы выбрали билеты:", async function () {
+  const actual = await getText(this.page,"h2");
+    const expected = "Вы выбрали билеты:";
+    expect(actual).contain(expected);
+});
+
+Then("кнопка Забронировать не активна", async function () {
+  const button = await this.page.$eval(
+    ".acceptin-button",
+    (el) => el.disabled
+  );
+  expect(button).equal(true);
 });
--- gh.test.js
+++ gh.test.js
@@ -1,11 +1,11 @@
 const { clickElement, getText} = require("./lib/commands.js");
 let page;
 beforeEach(async () => {
-  let day = 4;
+  let day = 3;
   page = await browser.newPage();
   await page.goto("https://qamid.tmweb.ru/client/index.php"); 
   await clickElement(page, `body > nav > a:nth-child(${day}) > span.page-nav__day-week`);
-  await clickElement(page, "body > main > section > div.movie-seances__hall > ul > li > a");
+  await clickElement(page, "[data-seance-id='156']");
 }, 30000);
 
 afterEach(() => {
@@ -16,8 +16,7 @@ describe("Booking",() => {
     let row = 4;
     let place1 = 4;
     await clickElement(page,`div.buying-scheme__wrapper > div:nth-child(${row}) > span:nth-child(${place1})`);
-    await clickElement(page,"body > main > section > button");
-    await clickElement(page,"p.ticket__hint:nth-child(9)");
+    await clickElement(page,".acceptin-button");
     const actual = await getText(page,"h2");
     const expected = "Вы выбрали билеты:";
     expect(actual).toContain(expected);
@@ -29,8 +28,7 @@ describe("Booking",() => {
     let place3 = 6;
     await clickElement(page,`div.buying-scheme__wrapper > div:nth-child(${row}) > span:nth-child(${place2})`);
     await clickElement(page,`div.buying-scheme__wrapper > div:nth-child(${row}) > span:nth-child(${place3})`);
-    await clickElement(page,"body > main > section > button");
-    await clickElement(page,"p.ticket__hint:nth-child(9)");
+    await clickElement(page,".acceptin-button");
     const actual = await getText(page,"h2");
     const expected = "Вы выбрали билеты:";
     expect(actual).toContain(expected);
@@ -38,10 +36,16 @@ describe("Booking",() => {
 
   test("Booking someone else's seat", async () => {
     let row = 2;
-    let place4 = 2;
+    let place4 = 3;
+    let day = 3;
     await clickElement(page,`div.buying-scheme__wrapper > div:nth-child(${row}) > span:nth-child(${place4})`);
-    const expected = await page.$eval("body > main > section > button",(el) => el.disabled
+    await clickElement(page,".acceptin-button");
+    await clickElement(page,".acceptin-button");
+    await page.goto("https://qamid.tmweb.ru/client/index.php"); 
+    await clickElement(page, `body > nav > a:nth-child(${day}) > span.page-nav__day-week`);
+    await clickElement(page, "[data-seance-id='156']");
+    const button = await page.$eval(".acceptin-button",(el) => el.disabled
     );
-    expect(expected).toEqual(true);
+    expect(button).toEqual(true);
   }, 30000);
 })
